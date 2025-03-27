6d9b6167a71617db8681fbe307206ba97a90b8aa
nikitha-git05 <nikithalokesh5@gmail.com>
2025-03-27 14:42:46 +0530

    Create terraform-pipeline.groovy

---
+++ terraform-pipeline.groovy
@@ -0,0 +1,46 @@
+pipeline {
+    agent any  // Runs on any available Jenkins agent
+
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git 'https://github.com/nikitha-git05/soure-file.git'  // Replace with your repo
+            }
+        }
+
+       
+
+        stage('Terraform Init') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                sh 'terraform plan -out=tfplan'
+            }
+        }
+
+        stage('Terraform Apply') {
+            when {
+                branch 'main' // Apply only on the main branch
+            }
+            steps {
+                sh 'terraform apply -auto-approve tfplan'
+            }
+        }
+
+        
+    }
+
+    post {
+        always {
+            archiveArtifacts artifacts: '**/*.tfstate', fingerprint: true
+        }
+        failure {
+            echo 'Terraform execution failed! Check logs for details.'
+        }
+    }
+}
